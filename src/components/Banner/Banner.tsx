b83dee165e723fce882b449cf7e258a4dbe99314
Alex Wong <alex.wong.aguilar@gmail.com>
2023-11-20 19:10:30 -0400

    Update: header, navbar, transitionPage, banner, floatedSearch

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -1,5 +1,21 @@
+import { FloatedSearch } from "../FloatedSearch";
+
 export function Banner() {
-    return (
-        <p>Banner</p>
-    )
+	return (
+		<div className="container relative mx-auto">
+			<div className="pt-20 md:pt-0 min-h-[80vh] bg-banner bg-no-repeat bg-center rounded-3xl relative flex flex-col items-center md:justify-center">
+				<div className="max-w-3xl text-center text-white">
+					<h2 className="text-5xl font-semibold">
+						Descubre la vivienda ideal para tu familia
+					</h2>
+					<p className="mt-2 text-xl md:mt-8">
+						Con más de 30 años de experiencia, en WaAlex Real Estate
+						contamos con los mejores profesionales para lo que
+						necesitas.
+					</p>
+				</div>
+				<FloatedSearch />
+			</div>
+		</div>
+	);
 }
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,42 @@
-"use client"
+"use client";
+
+import Link from "next/link";
+import { Navbar } from "../Navbar";
+import { useState } from "react";
+import { CiMenuFries } from "react-icons/ci";
+import { BsTelephone } from "react-icons/bs";
 
 export function Header() {
+	const [openMobileMenu, setOpenMobileMenu] = useState(false);
 
-    return (
-        <p>Header</p>
-    )
+	return (
+		<div className="container mx-auto my-5">
+			<div className="flex items-center justify-between px-5 md:px-0">
+				<Link href="/">
+					<h1 className="font-medium">WaAlex Real Estate</h1>
+				</Link>
+
+				<CiMenuFries
+					className="block text-2xl md:hidden"
+					onClick={() => setOpenMobileMenu(!openMobileMenu)}
+				/>
+
+				<Navbar openMobileMenu={openMobileMenu} />
+
+				<div className="flex items-center gap-2 md:gap-5">
+					<Link
+						href="tel:78945612"
+						className="flex items-center gap-4 cursor-pointer">
+						<BsTelephone />
+						<span className="hidden md:block">+591 78945612</span>
+					</Link>
+					<Link
+						href="/Login"
+						className="px-3 py-2 text-white rounded-lg bg-secondary hover:bg-black">
+						Login
+					</Link>
+				</div>
+			</div>
+		</div>
+	);
 }

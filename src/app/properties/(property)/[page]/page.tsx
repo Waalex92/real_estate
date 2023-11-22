e0f8f0ca4e64b19bc557fdb9fc7d2f9cc1cbd733
Alex Wong <alex.wong.aguilar@gmail.com>
2023-11-22 18:15:09 -0400

    Update:[property, form, 404page] components structure

--- src/app/properties/(property)/[page]/page.tsx
+++ src/app/properties/(property)/[page]/page.tsx
@@ -1,10 +1,24 @@
-'use client'
+"use client";
+
+import { Error404 } from "@/components/Error404";
+import { Header } from "@/components/Header";
+import { dataProperties } from "@/components/Properties/Properties.data";
+import { Property } from "@/components/Property";
+import { TransitionPage } from "@/components/TransitionPage";
+import { useParams } from "next/navigation";
 
 export default function Page() {
+	const router = useParams();
+	const propertyId = router.page;
+	const filteredPage = dataProperties.find(
+		(property) => property.id === Number(propertyId)
+	);
 
-    return (
-        <>
-            <p>Page</p>
-        </>
-    )
+	return (
+		<>
+			<TransitionPage />
+			<Header />
+			{filteredPage ? <Property house={filteredPage} /> : <Error404 />}
+		</>
+	);
 }

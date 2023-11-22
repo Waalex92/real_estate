d55c85dfdea11475c269561cf0ed74f032bcdfc7
Alex Wong <alex.wong.aguilar@gmail.com>
2023-11-22 16:11:15 -0400

    Update: [Properties, Search Components, Services, Slider, Transition] components structure

--- src/components/SearchLocation/SearchLocation.tsx
+++ src/components/SearchLocation/SearchLocation.tsx
@@ -1,10 +1,26 @@
-import { useState } from 'react'
-import { GrFormDown, GrLocation, GrFormUp } from 'react-icons/gr'
+import { useState } from "react";
+import { GrFormDown, GrLocation, GrFormUp } from "react-icons/gr";
 
 export function SearchLocation() {
-    const [isOpen, setIsOpen] = useState(false)
+	const [isOpen, setIsOpen] = useState(false);
 
-    return (
-        <p>Search Location</p>
-    )
+	return (
+		<div
+			className="relative mb-2 md:mb-0 flex items-center gap-4 border-[1px] rounded-lg px-3 py-2 justify-between cursor-pointer"
+			onClick={() => setIsOpen(!isOpen)}>
+			<GrLocation />
+			<div>
+				<p>Localización</p>
+				<p className="text-xs">Selecciona tu localización</p>
+			</div>
+			{isOpen ? <GrFormUp /> : <GrFormDown />}
+			{isOpen && (
+				<div className="absolute top-[70px] bg-white z-50 p-4 rounded-lg w-[230px] left-0">
+					<p>Madrid</p>
+					<p>Barcelona</p>
+					<p>Valencia</p>
+				</div>
+			)}
+		</div>
+	);
 }
--- src/components/SearchPriceRange/SearchPriceRange.tsx
+++ src/components/SearchPriceRange/SearchPriceRange.tsx
@@ -2,9 +2,27 @@ import { useState } from "react";
 import { GrFormDown, GrTag, GrFormUp } from "react-icons/gr";
 
 export function SearchPriceRange() {
-    const [isOpen, setIsOpen] = useState(false)
+	const [isOpen, setIsOpen] = useState(false);
 
-    return (
-        <p>Search price Range</p>
-    )
+	return (
+		<div
+			className="relative mb-2 md:mb-0 flex items-center gap-4 border-[1px] rounded-lg px-3 py-2 justify-between cursor-pointer"
+			onClick={() => setIsOpen(!isOpen)}>
+			<GrTag />
+			<div>
+				<p>Rango de precio</p>
+				<p className="text-xs">Selecciona tu rango de precio</p>
+			</div>
+			{isOpen ? <GrFormUp /> : <GrFormDown />}
+			{isOpen && (
+				<div className="absolute top-[70px] bg-white z-50 p-4 rounded-lg w-[230px] left-0">
+					<p>Cualquier rango</p>
+					<p>15.000$ - 25.000$</p>
+					<p>25.000$ - 35.000$</p>
+					<p>45.000$ - 55.000$</p>
+					<p>55.000$ - 65.000$</p>
+				</div>
+			)}
+		</div>
+	);
 }
